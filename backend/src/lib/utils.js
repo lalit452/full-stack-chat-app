696e3133cc5a20e39a723ea3b71b655501ea919b
lalit452 <lalitkumarmeena452@gmail.com>
2025-03-16 23:50:11 +0530

    Fixed MongoDB timeout issue

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -5,12 +5,20 @@ const token = jwt.sign({userId}, process.env.JWT_SECRET,{
     expiresIn : "7d"
 })
 
+// res.cookie("jwt", token, {
+//     maxAge: 7 * 24 * 60 * 60 * 1000,  //MS
+//     httpOnly: true, //prevent XSS attacks corss-site scripting attack
+//     sameSite: "strict", //CSRF attacks cross-site forgery attacks
+//     secure: process.env.NODE_ENV !== "development"  // jab tak devvelopment me rahenge ye false rahega
+// });
+
 res.cookie("jwt", token, {
-    maxAge: 7 * 24 * 60 * 60 * 1000,  //MS
-    httpOnly: true, //prevent XSS attacks corss-site scripting attack
-    sameSite: "strict", //CSRF attacks cross-site forgery attacks
-    secure: process.env.NODE_ENV !== "development"  // jab tak devvelopment me rahenge ye false rahega
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+    httpOnly: true,
+    sameSite: "lax", // Prevents CSRF but allows easier authentication
+    secure: process.env.NODE_ENV === "production"
 });
+
 
 return token;
 } 
